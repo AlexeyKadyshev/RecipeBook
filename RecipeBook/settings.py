fe24ff65517b8ca8e75d9b9b5f995245b8349522
Alexey <A.Kadyshev@yandex.ru>
2024-05-09 22:22:51 +0300

    add ch

--- RecipeBook/settings.py
+++ RecipeBook/settings.py
@@ -28,7 +28,7 @@ CSRF_COOKIE_SECURE = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
-    'alexeykd0404.pythonanywhere.com',
+    'alexeykkd0404.pythonanywhere.com',
 ]
 
 INTERNAL_IPS = [
@@ -112,10 +112,10 @@ WSGI_APPLICATION = 'RecipeBook.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'alexeykd0404$default',
-        'USER': 'alexeykd0404',
+        'NAME': 'alexeykkd0404$default',
+        'USER': 'alexeykkd0404',
         'PASSWORD': os.getenv('MYSQL_PASSWORD'),
-        'HOST': 'alexeykd0404.mysql.pythonanywhere-services.com',
+        'HOST': 'alexeykkd0404.mysql.pythonanywhere-services.com',
         'OPTIONS': {
             'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4',
